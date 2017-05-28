604c3412b78ea269daf7518442d698daa836618d
shiotomo <t.shiota16@gmail.com>
2017-05-28 21:31:52 +0900

    Revert "Add Neovim setup"
    This reverts commit d351f520b215c96ec72a8a4578eae5114a0c296c.

--- autoload/plugins/neobundle.vim
+++ autoload/plugins/neobundle.vim
@@ -32,8 +32,6 @@ NeoBundle 'Yggdroot/indentLine'
 NeoBundle 'mipmip/vim-minimap'
 "NeoBundle 'koron/minimap-vim'
 
-
-
 let g:lightline = {
       \ 'colorscheme': 'default',
       \ 'active': {
--- start.sh
+++ start.sh
@@ -3,11 +3,3 @@ mkdir ~/.vim/bundle
 mkdir ~/.vim/help
 git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 git clone git@github.com:vim-jp/vimdoc-ja.git ~/.vim/help
-
-ln -s .vimrc ~
-ln -s .init.vim ~/.config/nvim/
-
-curl -flo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-
