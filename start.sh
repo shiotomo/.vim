483f934c908b49b9544468edaec877c82e2c2176
shiotomo <t.shiota16@gmail.com>
2017-05-29 00:40:14 +0900

    change start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,9 @@ mkdir ~/.vim/help
 
 git clone git@github.com:vim-jp/vimdoc-ja.git ~/.vim/help
 
-ln -s .vimrc ~
+cd
+
+ln -s ~/.vim/.vimrc
 
 curl -flo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
