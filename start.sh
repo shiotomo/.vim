479c525ed14cd54f032f17c4a7b5cba7c4f8e1e8
shiotomo <t.shiota16@gmail.com>
2017-06-06 03:43:05 +0900

    Add .vimshrc

--- start.sh
+++ start.sh
@@ -7,6 +7,7 @@ git clone git@github.com:vim-jp/vimdoc-ja.git ~/.vim/help
 cd
 
 ln -s ~/.vim/.vimrc
+ln -s ~/.vim/.vimshrc
 
 curl -flo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
